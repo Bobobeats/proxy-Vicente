2414652c1e5cd831550de534b2c3edd68804a21b
7FFF00 <t42please@gmail.com>
2020-02-08 14:54:00 -0800

    FIX bug in unsanitized address for comments microservice

--- server/ipAddresses.js
+++ server/ipAddresses.js
@@ -1,5 +1,5 @@
 module.exports = {
-  comments: 'http://35.163.180.83:4001/',
+  comments: 'http://35.163.180.83:4001',
   relatedTracks: 'http://44.226.147.90:1000',
   player: 'http://44.232.36.205:3002',
 };
